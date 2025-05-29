cc10da4cd1554e9578890d310c04a1021d92c514
Ramendra777 <ramendra335428@gmail.com>
2025-05-29 16:01:33 +0530

    done CarFilters

--- app/(main)/cars/page.jsx
+++ app/(main)/cars/page.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { CarFilters } from "./_components/car-filters";
+import { getCarFilters } from "@/actions/car-listing";
+import { CarListings } from "./_components/cars-listing";
 
-const CarPage = async () => {
+export const metadata = {
+  title: "Cars | CarGenie",
+  description: "Browse and search for your dream car",
+};
+
+export default async function CarsPage() {
+  // Fetch filters data on the server
+  const filtersData = await getCarFilters();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-4 gradient-title">Browse Cars</h1>
 
-export default CarPage
+      <div className="flex flex-col lg:flex-row gap-8">
+        {/* Filters Section */}
+        <div className="w-full lg:w-80 flex-shrink-0">
+          <CarFilters filters={filtersData.data} />
+        </div>
+
+        {/* Car Listings */}
+        <div className="flex-1">
+          <CarListings />
+        </div>
+      </div>
+    </div>
+  );
+}
