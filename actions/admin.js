e34b42f11cd414b13b09a624d224b08ef39b92d5
Ramendra777 <ramendra335428@gmail.com>
2025-05-29 23:08:48 +0530

    admin test drive maintained section done

---
+++ actions/admin.js
@@ -0,0 +1,22 @@
+"use server";
+
+import { serializeCarData } from "@/lib/helpers";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+
+export async function getAdmin() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  // If user not found in our db or not an admin, return not authorized
+  if (!user || user.role !== "ADMIN") {
+    return { authorized: false, reason: "not-admin" };
+  }
+
+  return { authorized: true, user };
+}
